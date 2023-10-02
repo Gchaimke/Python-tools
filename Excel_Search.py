c888e716f87d7538168767cdb1e72abfea391474
Chaim Gorbov <Gchaimke@gmail.com>
2023-10-02 16:24:34 +0300

    Update Excel_Search.py

--- Excel_Search.py
+++ Excel_Search.py
@@ -46,8 +46,8 @@ def start():
     a = a + [i for i in range(19070500129, 19070500154)] + [i for i in range(19070500154, 19070500179)]
     a = a + [i for i in range(19073000001, 19073000026)] + [i for i in range(19080500255, 19080500285)]
     a = a + [i for i in range(19081300153, 19081300193)] + [i for i in range(19040900122, 19040900132)]
-    start_path = "G:\\Advantech\\# Archive\\@Clients\\D-FEND\\Production\\Production 2019"
-    start_path2 = "G:\\Advantech\\# Archive\\@Clients\\D-FEND\\Production\\Production 2020"
+    start_path = "C:\\Users\\user\\Desktop\\Work Files"
+    start_path2 = "C:\\Users\\user\\Desktop\\Work Files2"
     file_list = search_files(start_path) + search_files(start_path2)
     save_to_txt("System SN", "RF SN")
     for search in a:
@@ -61,8 +61,8 @@ def start():
 def debug():
     a = [i for i in range(19081300155, 19081300160)] + [i for i in range(19072600032, 19072600036)] + [i for i in range(
         19072600018, 19072600021)]
-    start_path = "C:\\Users\\gchaim\\Desktop\\Excel Search\\debug"
-    start_path2 = "C:\\Users\\gchaim\\Desktop\\Excel Search\\debug2"
+    start_path = "C:\\Users\\user\\Desktop\\Excel Search\\debug"
+    start_path2 = "C:\\Users\\user\\Desktop\\Excel Search\\debug2"
     save_to_txt("System SN", "RF SN")
     file_list = search_files(start_path) + search_files(start_path2)
     for search in a:
